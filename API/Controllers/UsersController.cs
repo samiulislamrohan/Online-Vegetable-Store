3ce712c5fe9f60551a2528784c76ef7ebaabde66
samiulislamrohan <rohansamiulislam@gmail.com>
2024-12-30 08:19:34 +0600

    first-commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext dataContext):ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = dataContext.Users.ToList();
+            return users;
+
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = dataContext.Users.Find(id);
+            if (user == null) { return NotFound(); }
+            return user;
+
+        }
+
+    }
+}
